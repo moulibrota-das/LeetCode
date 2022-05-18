6dc311c96738a8d4e96e1d533e4991469744ec5b
Moulibrota Das <dasmoulibrota@gmail.com>
2022-05-18 23:15:53 +0530

    Time: 1013 ms (34.64%), Space: 180.3 MB (51.90%) - LeetHub

---
+++ 1192-critical-connections-in-a-network/1192-critical-connections-in-a-network.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    void dfs(int node, int parent, vector<int> &vis, vector<int> &tin, vector<int> &low, int &timer, vector<vector<int>>& adj, vector<vector<int>> &ans) {
+        vis[node] = 1; 
+        tin[node] = low[node] = timer++; 
+        
+        for(auto it: adj[node]) {
+            if(it == parent) continue;
+
+            if(!vis[it]) {
+                dfs(it, node, vis, tin, low, timer, adj,ans); 
+                low[node] = min(low[node], low[it]); 
+                if(low[it] > tin[node]) {
+                    ans.push_back({it,node});
+                }
+            } else {
+                low[node] = min(low[node], tin[it]); 
+            }
+        }
+    }
+    vector<vector<int>> criticalConnections(int n, vector<vector<int>>& connection) {
+        vector<int> tin(n, -1);
+        vector<int> low(n, -1); 
+        vector<int> vis(n, 0); 
+        vector<vector<int>> ans;
+        vector<vector<int>> adj(n);
+        int timer = 0; 
+        
+        for(auto it : connection) {
+            adj[it[0]].push_back(it[1]);
+            adj[it[1]].push_back(it[0]); 
+	    }
+        
+        for(int i = 0;i<n;i++) {
+            if(!vis[i]) {
+                dfs(i, -1, vis, tin, low, timer, adj,ans); 
+            }
+        }
+        
+        return ans;
+    }
+};
