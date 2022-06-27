c2e6780861b41b0c435e89c4ec381370cdf5c194
Moulibrota Das <dasmoulibrota@gmail.com>
2022-06-27 10:38:55 +0530

    Time: 60 ms (18.64%), Space: 13.7 MB (17.75%) - LeetHub

--- 1689-partitioning-into-minimum-number-of-deci-binary-numbers/1689-partitioning-into-minimum-number-of-deci-binary-numbers.cpp
+++ 1689-partitioning-into-minimum-number-of-deci-binary-numbers/1689-partitioning-into-minimum-number-of-deci-binary-numbers.cpp
@@ -4,6 +4,7 @@ public:
         int max_digit = 0;
         
         for(auto it:n){
+            if(it-'0' == 9) return 9;
             max_digit = max(max_digit, it - '0');
         }
         return max_digit;
