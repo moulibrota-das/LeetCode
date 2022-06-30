4e9d8ec3451f54a5d30ba06f05b21c28d4e1dcd6
Moulibrota Das <dasmoulibrota@gmail.com>
2022-06-30 22:06:44 +0530

    Time: 19 ms (35.76%), Space: 10.8 MB (92.09%) - LeetHub

--- 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.cpp
+++ 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.cpp
@@ -1,7 +1,9 @@
 class Solution {
 public:
     int minMoves2(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
+        // sort(nums.begin(), nums.end());
+        // we can find the median by only sort the median element in the n/2 position
+        nth_element(nums.begin(), nums.begin() + nums.size()/2, nums.end());
         
         long long avg = nums[nums.size()/2];
         long long ans = 0;
