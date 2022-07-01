d7423e602cb9eff9815ff82e0b0f4e8517e1e7dc
Moulibrota Das <dasmoulibrota@gmail.com>
2022-07-01 22:23:57 +0530

    Time: 109 ms (33.94%), Space: 16.9 MB (40.06%) - LeetHub

---
+++ 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    static bool comp(vector<int>& a, vector<int>& b){
+        return a[1] >= b[1];
+    }
+    int maximumUnits(vector<vector<int>>& boxTypes, int truckSize) {
+        int ans = 0, k = truckSize;
+        sort(boxTypes.begin(), boxTypes.end(), comp);
+        
+        for(auto it:boxTypes){
+            if(k >= it[0]){
+                ans += it[0] * it[1];
+                k -= it[0];
+            }
+            else if(k < it[0]){
+                ans += k * it[1];
+                break;
+            }
+            
+            if(k == 0) break;
+        }
+        
+        return ans;
+    }
+};
