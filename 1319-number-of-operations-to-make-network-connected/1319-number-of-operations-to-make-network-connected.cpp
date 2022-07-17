2bcc804bdf0889ef420e4f8be273445f2713708e
Moulibrota Das <dasmoulibrota@gmail.com>
2022-07-17 12:33:30 +0530

    Time: 368 ms (5.58%), Space: 56.6 MB (17.08%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
@@ -1,14 +1,11 @@
 class Solution {
 public:
-    void dfs(int i, int p, vector<int>& vis, vector<vector<int>>& adj, int& count){
+    void dfs(int i, int p, vector<int>& vis, vector<vector<int>>& adj){
         vis[i] = 1;
         
         for(auto it:adj[i]){
             if(!vis[it]){
-                dfs(it, i, vis, adj, count);
-            }
-            else if(it != p){
-                count++;
+                dfs(it, i, vis, adj);
             }
         }
     }
@@ -24,10 +21,10 @@ public:
             adj[it[1]].push_back(it[0]);
         }
         
-        int comp = 0, count = 0;
+        int comp = 0;
         for(int i=0; i<n; i++){
            if(!vis[i]){
-               dfs(i,-1,vis,adj,count);
+               dfs(i,-1,vis,adj);
                comp++;
            }
         }
