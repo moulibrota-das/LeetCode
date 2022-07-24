5c81ec7f1a1f084873d6917e9c0da81a225b6e2b
Moulibrota Das <dasmoulibrota@gmail.com>
2022-07-24 22:46:12 +0530

    Time: 283 ms (53.42%), Space: 13.9 MB (76.05%) - LeetHub

--- 127-word-ladder/127-word-ladder.cpp
+++ 127-word-ladder/127-word-ladder.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
-        set<string> set;
+        unordered_set<string> set;
         for(auto it:wordList) set.insert(it);
         
         if(set.count(endWord) == 0) return 0;
@@ -17,10 +17,7 @@ public:
             
             while(n--){
                 string cur = q.front();
-                q.pop();
-                
-                if(cur == endWord) return depth;
-                
+                q.pop();                
                 
                 for(int i=0; i<cur.size(); i++){
                     string temp = cur;
@@ -28,12 +25,12 @@ public:
                     for(char ch = 'a'; ch <= 'z'; ch++){
                         temp[i] = ch;
                         
-                        if(set.count(temp) > 0){
+                        if(temp == endWord) return depth;
+                        
+                        if(temp != cur && set.count(temp) > 0){
                             q.push(temp);
                             set.erase(temp);
                         }
-                        
-                        if(temp == endWord) return depth;
                     }
                 }
             }
