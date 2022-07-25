bc0e8c21159af01d565a1055dee9675feebfa81b
Moulibrota Das <dasmoulibrota@gmail.com>
2022-07-25 13:23:52 +0530

    Time: 16 ms (33.20%), Space: 13.6 MB (63.15%) - LeetHub

---
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        int l=0, r= nums.size()-1;
+        int ansL = -1, ansR = -1;
+        
+        while(l <= r){
+            int mid = (l+r)/2;
+            
+            if(nums[mid] == target){
+                ansL = mid;
+            }
+            
+            if(nums[mid] >= target){
+                r = mid-1;
+            }
+            else{
+                l = mid + 1;
+            }
+        }
+        
+        l = 0, r = nums.size() - 1;
+        while(l <= r){
+            int mid = (l+r)/2;
+            
+            if(nums[mid] == target){
+                ansR = mid;
+            }
+            
+            if(nums[mid] <= target){
+                l = mid+1;
+            }
+            else{
+                r = mid - 1;
+            }
+        }
+        
+        return {ansL, ansR};
+    }
+};
