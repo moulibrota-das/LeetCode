5aded5d1968afdda53284d58fc6828f4abb37c98
Moulibrota Das <dasmoulibrota@gmail.com>
2022-08-09 20:36:23 +0530

    Time: 35 ms (52.69%), Space: 13 MB (42.04%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -15,8 +15,23 @@ public:
         return dp[i][j] = max(take1, max(take2, sp));
     }
     
-    int longestCommonSubsequence(string text1, string text2) {
-        vector<vector<int>> dp(text1.size(), vector<int>(text2.size(), -1));
-        return solve(0,0,text1,text2, dp);
+    int longestCommonSubsequence(string nums1, string nums2) {
+        int n = nums1.size(), m = nums2.size();
+        vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
+        int ans = 0;
+        
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=m; j++){
+                if(nums1[i-1] == nums2[j-1]){
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                    ans = max(ans,dp[i][j]);
+                }
+                else{
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+                    ans = max(ans, dp[i][j]);
+                }
+            }
+        }
+        return ans;
     }
 };
