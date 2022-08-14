3a233cc57a364abbdd699b4f1697088a24e82596
Moulibrota Das <dasmoulibrota@gmail.com>
2022-08-14 11:34:22 +0530

    Time: 14 ms (75.95%), Space: 9 MB (39.00%) - LeetHub

--- 72-edit-distance/72-edit-distance.cpp
+++ 72-edit-distance/72-edit-distance.cpp
@@ -21,29 +21,27 @@ public:
         return dp[i][j] = ans;
     }
     int minDistance(string word1, string word2) {
-        vector<vector<int>> dp(word1.size()+1, vector<int>(word2.size()+1, -1));
-        return solve(word1.size()-1, word2.size()-1, word1, word2, dp);
+        vector<vector<int>> dp(word1.size()+1, vector<int>(word2.size()+1, 0));
         
-//         for(int i=0; i<=word1.size(), i++)
-//             dp[i][0] = i;
+        for(int i=0; i<=word1.size(); i++)
+            dp[i][0] = i;
         
-//         for(int i=0; i<=word2.size(), i++)
-//             dp[0][i] = i;
+        for(int i=0; i<=word2.size(); i++)
+            dp[0][i] = i;
         
-//         for(int i=0; i<word1.size(); i++){
-//             for(int j=0; j<word2.size(); j++){
-//                 if(word1[i] == word2[j]){
-//                    dp[i+1][j+1] = dp[i][j];
-//                 }
-//                 else{
-//                     int rep = dp[i][j],word1, word2, dp);
-//                     int ins = solve(i,j+1, word1, word2, dp);
-//                     int del = solve(i+1,j,word1, word2, dp);
-
-//                     ans = 1 + min(rep, min(ins,del));
-//                 }
-//             }
-//         }
-        
+        for(int i=1; i<=word1.size(); i++){
+            for(int j=1; j<=word2.size(); j++){
+                if(word1[i-1] == word2[j-1])
+                    dp[i][j] = dp[i-1][j-1];
+                else{
+                    int rep = dp[i-1][j-1];
+                    int ins = dp[i][j-1];
+                    int del = dp[i-1][j];
+                    
+                    dp[i][j] = 1 + min(rep, min(ins,del));
+                }
+            }
+        }
+        return dp[word1.size()][word2.size()];
     }
 };
