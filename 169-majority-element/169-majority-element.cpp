d8a209d69566b7b818d770da2be11cc01c1f1761
Moulibrota Das <dasmoulibrota@gmail.com>
2022-08-18 21:54:22 +0530

    Time: 33 ms (45.11%), Space: 19.6 MB (13.58%) - LeetHub

---
+++ 169-majority-element/169-majority-element.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int ans = -1, count = 0;
+        
+        for(auto it:nums){
+            if(count == 0){
+                ans = it;
+                count++;
+            }
+            else if(it == ans){
+                count++;
+            }
+            else{
+                count--;
+            }
+        }
+        
+        return ans;
+    }
+};
