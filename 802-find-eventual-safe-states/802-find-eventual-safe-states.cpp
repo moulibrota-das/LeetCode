f0199b1ee3d1b545ff1ec3fa1798ea949a921cfc
Moulibrota Das <dasmoulibrota@gmail.com>
2022-08-21 11:40:13 +0530

    Time: 135 ms (99.98%), Space: 47.9 MB (49.95%) - LeetHub

---
+++ 802-find-eventual-safe-states/802-find-eventual-safe-states.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    bool dfs(int i, vector<int>& vis, vector<int>& res, vector<vector<int>>& graph){
+        vis[i] = 1;
+        res[i] = 1;
+        
+        for(auto it:graph[i]){
+            if(vis[it]==0 && dfs(it, vis, res, graph)){
+                continue;
+            }
+            else if(res[it] == 1 || vis[it] == 1){
+                return false;
+            }
+        }
+        res[i] = 0;
+        vis[i] = 2;
+        return true;
+    }
+    
+    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+        int n = graph.size();
+        vector<int> vis(n, 0);
+        vector<int> res(n, 0);
+        vector<int> ans;
+        
+        for(int i=0; i<n; i++){
+            if(vis[i] == 0 && dfs(i,vis, res, graph)){
+                ans.push_back(i);
+            }
+            else if(vis[i] == 2){
+                ans.push_back(i);
+            }
+        }
+        
+        
+        return ans;
+    }
+};
