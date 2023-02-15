014a8f1e57606d457d84c08d55716da0a174cf66
Moulibrota Das <dasmoulibrota@gmail.com>
2023-02-15 17:20:19 +0530

    Added solution - LeetHub

---
+++ Shortest path in Directed Acyclic Graph - GFG/shortest-path-in-directed-acyclic-graph.cpp
@@ -0,0 +1,73 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution {
+  public:
+     vector<int> shortestPath(int N,int M, vector<vector<int>>& edges){
+        // code here
+        vector<vector<pair<int,int>>> adj(N);
+        for(auto it:edges){
+            adj[it[0]].push_back({it[1], it[2]});
+        }
+        
+        queue<pair<int,int>> q;
+        vector<int> dis(N, INT_MAX);
+        
+        dis[0] = 0;
+        q.push({0, 0});
+        
+        while(!q.empty()){
+            int u = q.front().first;
+            int v = q.front().second;
+            q.pop();
+            
+            for(auto it:adj[u]){
+                if(dis[it.first] > v+it.second){
+                    dis[it.first] = v+it.second;
+                    q.push({it.first, v+it.second});
+                }
+            }
+        }
+        
+        for(int i=0; i<N; i++){
+            if(dis[i] == INT_MAX){
+                dis[i] = -1;
+            }
+        }
+        
+        return dis;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, m;
+        cin >> n >> m;
+        vector<vector<int>> edges;
+        for(int i=0; i<m; ++i){
+            vector<int> temp;
+            for(int j=0; j<3; ++j){
+                int x; cin>>x;
+                temp.push_back(x);
+            }
+            edges.push_back(temp);
+        }
+        Solution obj;
+        vector<int> res = obj.shortestPath(n, m, edges);
+        for (auto x : res) {
+            cout << x << " ";
+        }
+        cout << "\n";
+    }
+}
+
+// } Driver Code Ends
